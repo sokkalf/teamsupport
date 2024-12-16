cfed0b4caaca20aaaa3848c1484a18a5f5bd730b
Christian Lønaas <christian.lonaas@ugle-z.no>
2024-12-16 19:47:25 +0100

    Add Eivind mode

--- src/Main.elm
+++ src/Main.elm
@@ -1,19 +1,23 @@
 module Main exposing (main)
 
 import Browser
-import Html exposing (Html, div, text, img)
-import Html.Attributes exposing (style)
-import Time exposing (Posix, Zone)
 import Date
+import Html exposing (Html, div, img, input, label, strong, text, span, p)
+import Html.Attributes exposing (class, checked, style, type_)
+import Html.Events exposing (onCheck)
 import Task
+import Time exposing (Posix, Zone)
+
 
 -- MODEL
 
 type alias Model =
-    { currentName : String }
+    { currentName : String
+    , allNames : List String
+    , showAll : Bool
+    }
 
 
--- A list of names that we pick from based on the week number.
 names : List String
 names =
     [ "HC"
@@ -27,12 +31,12 @@ names =
 
 type Msg
     = GotTime Zone Posix
+    | ToggleShowAll Bool
 
 
 init : () -> ( Model, Cmd Msg )
 init _ =
-    -- We request the current time at initialization
-    ( { currentName = "Loading..." }
+    ( { currentName = "Loading...", allNames = names, showAll = False }
     , getCurrentTime
     )
 
@@ -40,8 +44,11 @@ init _ =
 getCurrentTime : Cmd Msg
 getCurrentTime =
     let
-        timeZone = Time.here -- get the local timezone
-        currentTime = Time.now
+        timeZone =
+            Time.here
+
+        currentTime =
+            Time.now
     in
     Task.perform identity (Task.map2 GotTime timeZone currentTime)
 
@@ -53,15 +60,21 @@ update msg model =
     case msg of
         GotTime zone posix ->
             let
-                weekNumber = (Date.weekNumber (Date.fromPosix zone posix))
+                weekNumber =
+                    Date.weekNumber (Date.fromPosix zone posix)
+
                 chosenName =
                     let
-                        index = (modBy (List.length names) weekNumber)
+                        index =
+                            modBy (List.length names) weekNumber
                     in
                     List.head (List.drop index names)
                         |> Maybe.withDefault "No name found"
             in
             ( { model | currentName = chosenName }, Cmd.none )
+
+        ToggleShowAll newVal ->
+            ( { model | showAll = newVal }, Cmd.none )
 
 
 -- VIEW
@@ -69,7 +82,7 @@ update msg model =
 view : Model -> Html Msg
 view model =
     div
-        [ style "height" "100vh"
+        [ style "height" "100em"
         , style "display" "flex"
         , style "flex-direction" "column"
         , style "margin" "0"
@@ -81,21 +94,37 @@ view model =
             , style "padding" "1em"
             , style "display" "flex"
             , style "align-items" "center"
-            , style "justify-content" "center"
+            , style "justify-content" "space-between"
             , style "font-family" "sans-serif"
             , style "font-size" "1em"
             , style "font-weight" "bold"
             ]
-            [ img
-                [ Html.Attributes.src "img/gyldendal.png"
-                , style "height" "32px"
-                , style "margin-right" "1em"
+            [ div
+                [ style "display" "flex"
+                , style "align-items" "center"
                 ]
-                []
-            , text "Spiroobar"
-            ]
+                [ img
+                    [ Html.Attributes.src "img/gyldendal.png"
+                    , style "height" "32px"
+                    , style "margin-right" "1em"
+                    ]
+                    []
+                , text "Spiroobar"
+                ]
+            , div [] [
+                span [ style "padding-right" "0.75em" ] [ text "Eivind mode" ]
+                , label [ class "switch" ]
+                [ input
+                    [ type_ "checkbox"
+                    , checked model.showAll
+                    , onCheck ToggleShowAll
+                    ]
+                    []
+                , span [ class "slider round" ] []
+                ]
+            ]]
 
-          -- Main content area
+        -- Main content area
         , div
             [ style "flex" "1"
             , style "display" "flex"
@@ -104,9 +133,39 @@ view model =
             , style "font-size" "3em"
             , style "font-family" "sans-serif"
             ]
-            [ text model.currentName ]
+            [ if model.showAll then
+                viewAllNames model
+              else
+                text model.currentName
+            ]
         ]
 
+
+viewAllNames : Model -> Html Msg
+viewAllNames model =
+    let
+        -- Helper function to turn each name into either bold or normal text
+        nameToHtml name =
+            if name == model.currentName then
+                strong [] [ text name ]
+            else
+                text name
+
+        -- Build the list of names separated by ", "
+        namesWithSeparators : List (Html Msg)
+        namesWithSeparators =
+            case model.allNames of
+                [] ->
+                    []
+
+                first :: rest ->
+                    nameToHtml first
+                        :: List.concatMap (\n -> [ text ", ", nameToHtml n ]) rest
+    in
+    div [] namesWithSeparators
+
+
+main : Program () Model Msg
 main =
     Browser.element
         { init = init
